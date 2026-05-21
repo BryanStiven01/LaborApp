e075f9a08fd7a005e30e34dae246bf16dd548b63
Pedro-Urbina-18 <168126481+Pedro-Urbina-18@users.noreply.github.com>
2026-05-21 16:24:58 -0600

    feat: modulo de sectores completado y verificado

---
+++ src/modules/sector/entities/sector.entity.ts
@@ -0,0 +1,22 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity('sectors')
+export class Sector {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', length: 100, unique: true })
+  name: string;
+
+  @Column({ type: 'text', nullable: true })
+  description: string;
+
+  @Column({ type: 'boolean', default: true })
+  status: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
